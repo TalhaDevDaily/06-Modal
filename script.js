1b9934dca34d7546ea637526979f1267d339a53f
Abu Talha Tamim <talha@boishakhikunja.onmicrosoft.com>
2025-07-16 23:21:20 +0600

    Modal [Done]

--- script.js
+++ script.js
@@ -5,8 +5,29 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+// Open Modal Funciton
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Open Modal Funciton
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+// Close Modal
+btnCloseModal.addEventListener('click', closeModal);
+// Close Overlay
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
